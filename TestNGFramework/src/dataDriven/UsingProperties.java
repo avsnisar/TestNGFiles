34b5a88b05b77333f703cfe4dd8e7a030b703754
Администратор <@XTREME-TJ60N2BP>
2019-07-08 17:04:16 +0300

    UsingProperties class updated imports; fix structure of xml configuration

--- TestNGFramework/src/dataDriven/UsingProperties.java
+++ TestNGFramework/src/dataDriven/UsingProperties.java
@@ -4,7 +4,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
